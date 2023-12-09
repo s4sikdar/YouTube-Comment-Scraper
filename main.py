14e0c74671540a559dda91973ea282b686662097
s4sikdar <saileshsikdar1@gmail.com>
2023-12-09 11:55:25 -0500

    Made changes to the bash script to be more flexible
    Added help messaging for the bash script, as well as the ability to
    specify arguments. Now the script can specify a help message with the -h
    argument (or --help or -help), and it can specify a name for the virtual
    environment directory if you want to use a different name than the
    default. Run "./setup --help" for help messaging.
    
    Started adding the argument parser in the main.py file to parse
    arguments using the argparse module.
    
    Disclaimer: I used the below stack overflow answer for help on how to
    use getopt.
    https://stackoverflow.com/questions/16483119/an-example-of-how-to-use-getopts-in-bash

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import argparse
 from iterators.comment_iterator import CommentIterator
 
+parser = argparse.ArgumentParser()
+parser.add_argument('-l', '--limit', type=int, default=None)
+parser.add_argument('--url', type=str, default=None, required=True)
+parser.add_argument('--pattern', type=str, default=None)
+parser.add_argument('-o', '--output', type=str, default=None)
+
 if __name__ == '__main__':
     print(0)
