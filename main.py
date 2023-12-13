679303f751f6b20eeb22b05a99ebab802c94a130
s4sikdar <saileshsikdar1@gmail.com>
2023-12-13 01:14:46 -0500

    Added more detail to the command line interface in main.py
    Added more detail to the command line interface in main.py, which will
    be the entry point in the application.

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 import argparse
 from iterators.comment_iterator import CommentIterator
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-l', '--limit', type=int, default=None)
-parser.add_argument('--url', type=str, default=None, required=True)
-parser.add_argument('--pattern', type=str, default=None)
-parser.add_argument('-o', '--output', type=str, default=None)
+parser = argparse.ArgumentParser(
+    description=(
+        'A YouTube comment scraper that scrapes the comments in the YouTube comments section for a video of your choice, matching a regular expression of your choosing.'
+        'The comments are scraped and added to the a JSON file whose name you can specify.'
+    )
+)
+
+parser.add_argument(
+    '-l', '--limit', type=int, default=None, help='the maximum number of comments we will scrape'
+)
+parser.add_argument(
+    '--url', type=str, default=None, required=True, help='the YouTube video url for the video you want to scrape (should be available)'
+)
+parser.add_argument(
+    '--pattern', type=str, default=None,
+    help='The regular expression pattern you use to parse and match text patterns in comments. By default, all comments are matched and added to the JSON.'
+)
+parser.add_argument(
+    '-o', '--output', type=str, default=None, help='The output file you will store the JSON in. Defaults to "comments.json".'
+)
 
 if __name__ == '__main__':
     print(0)
