7cab7fe4db141ffb37a8841b3b6b205392838250
s4sikdar <saileshsikdar1@gmail.com>
2023-12-13 13:13:03 -0500

    Finished adding entry point in main.py
    Fixed syntax issue in setup script as well, and updated the selectors
    for the change in CSS selector that was used in
    iterators/comment_iterator.py.

--- main.py
+++ main.py
@@ -1,26 +1,35 @@
 import argparse
+import json
 from iterators.comment_iterator import CommentIterator
 
-parser = argparse.ArgumentParser(
-    description=(
-        'A YouTube comment scraper that scrapes the comments in the YouTube comments section for a video of your choice, matching a regular expression of your choosing.'
-        'The comments are scraped and added to the a JSON file whose name you can specify.'
+def main():
+    parser = argparse.ArgumentParser(
+        description=(
+            'A YouTube comment scraper that scrapes the comments in the YouTube comments section for a video of your choice, matching a regular expression of your choosing.'
+            'The comments are scraped and added to the a JSON file whose name you can specify.'
+        )
     )
-)
-
-parser.add_argument(
-    '-l', '--limit', type=int, default=None, help='the maximum number of comments we will scrape'
-)
-parser.add_argument(
-    '--url', type=str, default=None, required=True, help='the YouTube video url for the video you want to scrape (should be available)'
-)
-parser.add_argument(
-    '--pattern', type=str, default=None,
-    help='The regular expression pattern you use to parse and match text patterns in comments. By default, all comments are matched and added to the JSON.'
-)
-parser.add_argument(
-    '-o', '--output', type=str, default=None, help='The output file you will store the JSON in. Defaults to "comments.json".'
-)
+    parser.add_argument(
+        '-l', '--limit', type=int, default=None, help='the maximum number of comments we will scrape'
+    )
+    parser.add_argument(
+        '--url', type=str, default=None, required=True, help='the YouTube video url for the video you want to scrape (should be available)'
+    )
+    parser.add_argument(
+        '--pattern', type=str, default=None,
+        help='The regular expression pattern you use to parse and match text patterns in comments. By default, all comments are matched and added to the JSON.'
+    )
+    parser.add_argument(
+        '-o', '--output', type=str, default='comments.json', help='The output file you will store the JSON in. Defaults to "comments.json".'
+    )
+    arguments = parser.parse_args()
+    comments = {
+        'comments': []
+    }
+    with open(arguments.output, 'w') as output_file:
+        for item in CommentIterator(arguments.url, arguments.limit, arguments.pattern):
+            comments['comments'].append(item)
+        output_file.write(json.dumps(comments))
 
 if __name__ == '__main__':
-    print(0)
+    main()
