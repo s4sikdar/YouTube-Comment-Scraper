4dc42c325fb99e12c3f8686a266820c01c4fad7e
s4sikdar <saileshsikdar1@gmail.com>
2023-12-19 19:30:12 -0500

    Removed chromedriver download process from Bash script
    Removed the chromedriver download process from the Bash script, instead
    offloading it onto a separate python script called "get_webdriver.py"
    that will run and install the relevant version of chromedriver based on
    a given platform name.
    
    The platform name is determined in this script using the uname command
    to get the operating system name and the machine architecture name. The
    script will then determine if you have a valid platform that is supported
    by chrome webdriver.
    
    The script will first check to see if you have chromedriver installed in
    your ~/bin/ directory, and if it is of the latest version. If it is,
    then don't do anything. Otherwise, the script will find out the relevant
    platform name and accordingly download the relevant version of chrome
    webdriver. From there, the script will unzip only the chromedriver
    binary.
    
    To do:
    * add the newly installed chromedriver binary to the ~/bin/ directory
    * add safeguards to ensure that the ~/bin/ directory is part of the
      path
    * connect this script to setup.sh. Add documentation for this new
      functionality
    * Consider adding a custom directory to your path that contains
      the downloaded chromedriver binary, and add functionality to remove
      this custom directory as well after you have used the script (by
      adding a flag in setup.sh to add the binary to a custom directory that
      will be added to path, and then adding a flag to remove that custom
      directory)
    
    Next commit:
    * python script that downloads the chromedriver binary

--- download_chromedriver.sh
+++ download_chromedriver.sh
@@ -1,4 +1,5 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
+
 
 # Function to get the operating system. Credit to the following link,
 # as I got the function from here: https://gist.github.com/prabirshrestha/3080525
@@ -23,17 +24,13 @@ function get_os() {
 	esac
 }
 
+
 # The function to get the latest stable version of chrome driver based on the following link below:
-# https://googlechromelabs.github.io/chrome-for-testing/
+# https://googlechromelabs.github.io/chrome-for-testing/ . First we try to use the python script to
+# try and download the binary file. If it fails, we try the ducktape hack in this function to hardcode the
+# url and make a curl request from there.
 function get_latest_chromedriver() {
-	latest_stable_version=$(curl 'https://googlechromelabs.github.io/chrome-for-testing/LATEST_RELEASE_STABLE' 2> /dev/null)
-	download_endpoint_base='https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/'
-	linux_64_zip_path='linux64/chromedriver-linux64.zip'
-	mac_arm64_zip_path='mac-arm64/chromedriver-mac-arm64.zip'
-	mac_x64_zip_path='mac-x64/chromedriver-mac-x64.zip'
-	win32_zip_path='win32/chromedriver-win32.zip'
-	win64_zip_path='win64/chromedriver-win64.zip'
-	path_end=''
+	platform=''
 	os_name=$(get_os)
 	#  unzip -l chromedriver_win32.zip | tail -n 4 | head -n 2 | awk '{print $4}'
 	machine_hardware_name=$(uname -m | tr '[:upper:]' '[:lower:]')
@@ -45,7 +42,7 @@ function get_latest_chromedriver() {
 		linux*)
 			if [ "${machine_hardware_name}" = 'x86_64' ]
 			then
-				path_end=${linux_64_zip_path}
+				platform='linux64'
 			else
 				echo "You are using linux on a machine that is not 64-bit. You must have a 64-bit machine to have a linux chromedriver. Exiting with an error code of 2." &>2
 				exit 2
@@ -54,10 +51,10 @@ function get_latest_chromedriver() {
 		darwin*)
 			if  [ "${machine_hardware_name}" = 'arm64' ]
 			then
-				path_end=${mac_arm64_zip_path}
+				platform='mac-arm64'
 			elif [ "${machine_hardware_name}" = 'x86_64' ]
 			then
-				path_end=${mac_x64_zip_path}
+				platform='mac-x64'
 			else
 				echo "You are using a Mac OS on a machine that is not an arm-64 machine or a 64-bit machine." &>2
 				echo "You must have one of the two to use the linux chromedriver on a Mac OS. Exiting with an error code or 2." &>2
@@ -65,12 +62,13 @@ function get_latest_chromedriver() {
 			fi
 			;;
 		windows*)
+			# If machine hardware name is one of the entries in the bit32 array, this is true
 			if  [ "${machine_hardware_name}" = "${bit32[0]}" -o "${machine_hardware_name}" = "${bit32[1]}" ]
 			then
-				path_end=${win32_zip_path}
+				platform='win32'
 			elif [ "${machine_hardware_name}" = 'x86_64' ]
 			then
-				path_end=${win64_zip_path}
+				platform='win64'
 			else
 				echo "You are using Windows on an unknown machine that does not support chromedriver. Exiting with an error code of 2" &>2
 				exit 2
@@ -81,19 +79,23 @@ function get_latest_chromedriver() {
 			exit 2
 			;;
 	esac
-	full_download_url_path="${download_endpoint_base}/${latest_stable_version}/${path_end}"
-	resulting_fname=$(echo ${path_end} | awk -F / '{ print $NF }')
-	echo "Downloading \"${full_download_url_path}\" to \"${resulting_fname}\" locally in your ~/bin/ directory."
-	curl \"${full_download_url_path}\" -o \"${resulting_fname}\"
-	result_command="curl \"${full_download_url_path}\" -o \"${resulting_fname}\""
-	echo ${result_command}
-	unzip_line_count=$(unzip -ql ${resulting_fname} | wc -l)
-	line_count_outside_last_2=$((${unzip_line_count} - 2))
-	file_count=$((${line_count_outside_last_2} - 2))
-	# get all files in the zip file that are not the chromedriver binary and leave them in the zip file
-	unzip -ql ${resulting_fname} | head -n ${line_count_outside_last_2} | tail -n ${file_count} | awk '{print $4}' | awk '!/chromedriver\.exe/ { print $NF }' > ${tempfile}
-	cat ${tempfile} | xargs unzip ${resulting_fname} -x
+	zip_fname=$(./get_webdriver.py --platform="${platform}" 2> /dev/null)
+	if [ -n ${zip_fname} ]
+	then
+		# unzip -ql ${zip_fname}
+		unzip_line_count=$(unzip -ql ${zip_fname} | wc -l)
+		line_count_outside_last_2=$((${unzip_line_count} - 2))
+		file_count=$((${line_count_outside_last_2} - 2))
+		# get all files in the zip file that are not the chromedriver binary and leave them in the zip file
+		unzip -ql ${zip_fname} | head -n ${line_count_outside_last_2} | tail -n ${file_count} | awk '/\/chromedriver(.exe)?$/ {print $NF}' > ${tempfile}
+		# This only unzips the chromedriver binary
+		cat ${tempfile} | xargs unzip ${resulting_fname} -x
+	else
+		echo "Was unable to download the latest chromedriver binary for some reason. Exiting with an error code of 1." &>2
+		exit 1
+	fi
 }
+
 
 # The function that ensures the latest stable chromedriver is in your $PATH environment variable.
 # Since ~/bin/ is often in the git $PATH environment variable, the function checks if a bin directory
@@ -141,5 +143,6 @@ function find_and_install_chromedriver() {
 		get_latest_chromedriver
 	fi
 }
+
 
 find_and_install_chromedriver
