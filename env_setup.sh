8517c8d340660f0408ed3d686ca389578136178f
s4sikdar <saileshsikdar1@gmail.com>
2023-12-25 10:47:45 -0500

    Changed command line argument interface and updated documentation
    Upon inspection, getopt is not compatible with Mac OS (stack overflow
    link: https://stackoverflow.com/questions/11777695/why-the-getopt-doesnt-work-well-in-my-mac-os).
    So I changed the script to use getopts instead. I changed the
    documentation to reflect this. When running "source ./env_setup.sh -h",
    it would exit with an exit code of 0, and since you used the source
    command, this change would take place on your bash terminal session.
    So I accounted for this, and now pressing -h will not close your
    terminal session. The same applies when you pass in unrecognized
    arguments, such as -q. Now if you run "source ./env_setup.sh -q",
    the script will not exit out of your current session. The script will
    print a message to standard error and do nothing else.

--- env_setup.sh
+++ env_setup.sh
@@ -12,15 +12,17 @@ virtual_env_name="virtual_env_dependencies"
 no_caching=false
 color=false
 filename="requirements.txt"
+continue_setup=true
+
 
 # Print the below help message
 function usage() {
 	# cat << EOF prints out text and stops printing the moment it matches the text found after '<<'
 	cat << ENDMSG
 Usage:
-./env_setup.sh [-e|-ename|--ename <virtual environment name>] [-n|-nocache|--nocache] [-c|-color|--color] [-h|-help|--help]
+./env_setup.sh [-e <virtual environment name>] [-n] [-c] [-h]
 OR
-source env_setup.sh [-e|-ename|--ename <virtual environment name>] [-n|-nocache|--nocache] [-c|-color|--color]
+source env_setup.sh [-e <virtual environment name>] [-n] [-c]
 # DO NOT send the -h flag when running source with this script. Your terminal session will close immediately.
 
 Description:
@@ -45,36 +47,32 @@ source ./\${DIRNAME}/Scripts/activate
 *************************************************************************************************************************************
 
 Arguments supported:
--h	-help		--help		Display this help message and exit immediately if this is specified.
+-h	Display this help message and exit immediately if this is specified.
 
--e	-ename		--ename		Specify the virtual environment directory name. By default this is "virtual_env_dependencies".
+-e	Specify the virtual environment directory name. By default this is "virtual_env_dependencies".
 
--n	-nocache	--nocache	Does not use cached packages when managing dependencies. This installs packages with the --no-cache-dir
-					flag, and purges the cache with "pip cache purge" when uninstalling dependencies. This will upgrade pip
-					with the --no-cache-dir flag as well. If you specify this, then the script will take more time and cause
-					significantly more network usage.
+-n	Does not use cached packages when managing dependencies. This installs packages with the --no-cache-dir
+	flag, and purges the cache with "pip cache purge" when uninstalling dependencies. This will upgrade pip
+	with the --no-cache-dir flag as well. If you specify this, then the script will take more time and cause
+	significantly more network usage.
 
--c	-color		--color		Print color messages. Print messages in yellow or green (yellow means some adjustment is being made,
-					green means the packages have all been installed successfully). This is disabled by default.
+-c	Print color messages. Print messages in yellow or green (yellow means some adjustment is being made,
+	green means the packages have all been installed successfully). This is disabled by default.
 
--f	-filename	--filename	Specify the filename that has the listed dependencies you install. This filename should be the output
-					of "pip freeze". By default it is "requirements.txt".
+-f	Specify the filename that has the listed dependencies you install. This filename should be the output
+	of "pip freeze". By default it is "requirements.txt".
 
 Examples:
 ./setup -h 					# Prints this help message
-./setup -ename=dirname 				# sets virtual environment directory name to be "dirname"
-./setup --ename=dirname				# same as above
 ./setup -e dirname				# same as above
 ./setup -n					# disable caching
-./setup -nocache				# disable caching
-./setup --nocache				# disable caching
-./setup --color					# print color messages
-./setup -color					# print color messages
 ./setup -c					# print color messages
-./setup -c --nocache --ename=dependencies	# disable caching, sets the virtual environment directory name to be "dependencies", and enables color messaging
+./setup -c -n -e dependencies			# disable caching, sets the virtual environment directory name to be "dependencies", and enables color messaging
 ./setup						# sets the virtual environment directory to be the default: "virtual_env_dependencies" (and continues from there)
+./setup -f dependencies.txt			# sets the dependency text file to be used to be "dependencies.txt". All else is default.
 ENDMSG
 }
+
 
 # Print text in color. One argument is allowed, which is the color code.
 # The text you want to change in color should be passed in as input to this function
@@ -95,6 +93,7 @@ function print_color() {
 }
 
 
+
 # Upgrade pip when an upgrade is available, --no-cache-dir is run to disable caching if that argument is specified
 function upgrade_pip() {
 	if [ "${1}" = true ]
@@ -104,6 +103,7 @@ function upgrade_pip() {
 		use_correct_python_version -m pip install --upgrade pip --quiet
 	fi
 }
+
 
 # Install dependencies with pip install. --no-cache-dir is run to disable caching if that argument is specified
 function install_dependencies() {
@@ -115,110 +115,117 @@ function install_dependencies() {
 	fi
 }
 
+#set -x
+OPTIND=1
 # The part of the script that parses arguments passed into the command line. The script uses getopt,
 # so you should try checking to see if getopt is available in your terminal by typing 'getopt --help'.
 if [ ${#} -ne 0 ]
 then
-	options=$(getopt -l "help,nocache,color,ename:" -o "hnce:" -a -- "${@}")
-	if [ ${?} -ne 0 ]
-	then
-		echo "Incorrect invocation of the script. Printing help message below and exiting with an error code of 1."
-		usage
-		exit 1
-	fi
-	eval set -- "${options}"
-	while true
+	#options=$(getopt -l "help,nocache,color,ename:" -o "hnce:" -a -- "${@}")
+	#if [ ${?} -ne 0 ]
+	#then
+	#	echo "Incorrect invocation of the script. Printing help message below and exiting with an error code of 1."
+	#	usage
+	#	exit 1
+	#fi
+	#eval set -- "${options}"
+	while getopts ":he:f:nc" arg_value
 	do
-	case "${1}" in
-		-h|--help)
-			usage
-			exit 0
-			break
-			;;
-		-e|--ename)
-			shift
-			virtual_env_name="${1}"
-			;;
-		-f|--filename)
-			shift
-			filename="${1}"
-			if [ ! -f "${filename}" ]
-			then
-				echo "${filename} does not exist. Using \"requirements.txt\"" | print_color "${YELLOW}"
-				filename="requirements.txt"
-			fi
-			;;
-		-n|--nocache)
-			no_caching=true
-			;;
-		-c|--color)
-			color=true
-			;;
-		--)
-			shift
-			break
-			;;
-
-	esac
-	shift
+		case ${arg_value} in
+			h)
+				usage
+				continue_setup=false
+				break
+				;;
+			e)
+				virtual_env_name="${OPTARG}"
+				;;
+			f)
+				filename="${OPTARG}"
+				if [ ! -f "${filename}" ]
+				then
+					echo "${filename} does not exist. Using \"requirements.txt\"" | print_color "${YELLOW}"
+					filename="requirements.txt"
+				fi
+				;;
+			n)
+				no_caching=true
+				;;
+			c)
+				color=true
+				;;
+			\?)
+				echo "Invalid usage. Run ./env_setup.sh -h for documentation on how to use this script" 1>&2
+				continue_setup=false
+				;;
+		esac
 	done
+	shift $((${OPTIND}-1))
 fi
+#set +x
 
-tempfile=$(mktemp)
-diff_output=$(mktemp)
-# Use the find command to search for the directory with the virtual environment directory name. Use the length of the directories matching to
-# determine if the directory exists.
-directories_matching=$(find ./ -type d -name "${virtual_env_name}")
-length=$(echo ${directories_matching} | wc -c)
-if [ ${length} -gt 1 ]
-then
-	# Directory was found, so try activating the script to start the virtual environment. If it doesn't work (non-zero return code), then
-	# remove this directory and install a new virtual environment with the same directory name. If it works, then check the existing package
-	# dependencies.
-	source ./${virtual_env_name}/Scripts/activate 2> /dev/null
-	if [ ${?} -ne 0 ]
+function run_setup() {
+	tempfile=$(mktemp)
+	diff_output=$(mktemp)
+	# Use the find command to search for the directory with the virtual environment directory name. Use the length of the directories matching to
+	# determine if the directory exists.
+	directories_matching=$(find ./ -type d -name "${virtual_env_name}")
+	length=$(echo ${directories_matching} | wc -c)
+	if [ ${length} -gt 1 ]
 	then
-		echo "Existing \"${virtual_env_name}\" directory found that is not a virtual environment directory." | print_color "${YELLOW}"
-		echo "This script will delete it, install a new virtual environment with directory name \"${virtual_env_name}\", and install dependencies." | print_color "${YELLOW}"
-		rm -r "./${virtual_env_name}"
+		# Directory was found, so try activating the script to start the virtual environment. If it doesn't work (non-zero return code), then
+		# remove this directory and install a new virtual environment with the same directory name. If it works, then check the existing package
+		# dependencies.
+		source ./${virtual_env_name}/Scripts/activate 2> /dev/null
+		if [ ${?} -ne 0 ]
+		then
+			echo "Existing \"${virtual_env_name}\" directory found that is not a virtual environment directory." | print_color "${YELLOW}"
+			echo "This script will delete it, install a new virtual environment with directory name \"${virtual_env_name}\", and install dependencies." | print_color "${YELLOW}"
+			rm -r "./${virtual_env_name}"
+			use_correct_python_version -m venv "./${virtual_env_name}"
+			source ./${virtual_env_name}/Scripts/activate
+			upgrade_pip "${no_caching}"
+			install_dependencies "${no_caching}"
+			echo "Requirements have been installed." | print_color "${GREEN}"
+		else
+			# Check the current package dependency listing and compare it with requirements.txt using the diff command to determine if there
+			# are any differences in package installed (i.e. different version, different packages installed, etc.)
+			use_correct_python_version -m pip freeze > ${tempfile}
+			diff ${tempfile} ./requirements.txt -ZEbB > ${diff_output}
+			lines_difference=$(wc -l ${diff_output} | awk '{ print $1 }')
+			if [ ${lines_difference} -gt 0 ]
+			then
+				echo "Existing package installations were found that differ from the dependencies in ${filename}" | print_color "${YELLOW}"
+				echo "Removing all existing dependencies and installing the dependencies in ${filename}" | print_color "${YELLOW}"
+				upgrade_pip "${no_caching}"
+				use_correct_python_version -m pip uninstall -r ${tempfile} -y --quiet
+				if [ "${no_caching}" = true ]
+				then
+					use_correct_python_version -m pip cache purge 2> /dev/null
+				fi
+				install_dependencies "${no_caching}"
+				echo "Requirements have been installed." | print_color "${GREEN}"
+			fi
+			upgrade_pip "${no_caching}"
+		fi
+	else
+		# If the length variable is 0, then there is no existing directory with the given virtual environment name. Create a virtual environment
+		# with this directory name and install all of the dependencies.
+		echo "No existing virtual environment found with directory name \"${virtual_env_name}\"." | print_color "${YELLOW}"
+		echo "Creating a new virtual environment with directory name \"${virtual_env_name}\" and installing the dependencies listed in ${filename}" | print_color "${YELLOW}"
 		use_correct_python_version -m venv "./${virtual_env_name}"
 		source ./${virtual_env_name}/Scripts/activate
 		upgrade_pip "${no_caching}"
 		install_dependencies "${no_caching}"
 		echo "Requirements have been installed." | print_color "${GREEN}"
-	else
-		# Check the current package dependency listing and compare it with requirements.txt using the diff command to determine if there
-		# are any differences in package installed (i.e. different version, different packages installed, etc.)
-		use_correct_python_version -m pip freeze > ${tempfile}
-		diff ${tempfile} ./requirements.txt -ZEbB > ${diff_output}
-		lines_difference=$(wc -l ${diff_output} | awk '{ print $1 }')
-		if [ ${lines_difference} -gt 0 ]
-		then
-			echo "Existing package installations were found that differ from the dependencies in ${filename}" | print_color "${YELLOW}"
-			echo "Removing all existing dependencies and installing the dependencies in ${filename}" | print_color "${YELLOW}"
-			upgrade_pip "${no_caching}"
-			use_correct_python_version -m pip uninstall -r ${tempfile} -y --quiet
-			if [ "${no_caching}" = true ]
-			then
-				use_correct_python_version -m pip cache purge 2> /dev/null
-			fi
-			install_dependencies "${no_caching}"
-			echo "Requirements have been installed." | print_color "${GREEN}"
-		fi
-		upgrade_pip "${no_caching}"
 	fi
-else
-	# If the length variable is 0, then there is no existing directory with the given virtual environment name. Create a virtual environment
-	# with this directory name and install all of the dependencies.
-	echo "No existing virtual environment found with directory name \"${virtual_env_name}\"." | print_color "${YELLOW}"
-	echo "Creating a new virtual environment with directory name \"${virtual_env_name}\" and installing the dependencies listed in ${filename}" | print_color "${YELLOW}"
-	use_correct_python_version -m venv "./${virtual_env_name}"
-	source ./${virtual_env_name}/Scripts/activate
-	upgrade_pip "${no_caching}"
-	install_dependencies "${no_caching}"
-	echo "Requirements have been installed." | print_color "${GREEN}"
-fi
 
-# Remove the tempfiles we create
-rm ${tempfile}
-rm ${diff_output}
+	# Remove the tempfiles we create
+	rm ${tempfile}
+	rm ${diff_output}
+}
+
+if [ "${continue_setup}" = true ]
+then
+	run_setup
+fi
