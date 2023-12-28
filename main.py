febf7ab545b22e411fe5ab57682e4cee12aae459
s4sikdar <saileshsikdar1@gmail.com>
2023-12-28 11:05:15 -0500

    Adding support for specified time limits
    I'm now adding support for time limits you can specify to have the
    script run for a certain time limit. The number of hours, minutes and
    seconds is combined to get the total second count that you want the
    script to run. The script won't run for that exact number of seconds. It
    will finish parsing the current comment (or current reply in a comment
    thread) and then stop scraping the rest of the comments on the video.
    
    I am also adding verification to ensure that the parameters passed in
    are valid inputs (i.e. comment limits are >= 0, time limits are > 0 if
    specified, etc.). There is also an issue with the bash setup script on
    the part where you download the latest version of chromedriver. I will
    be debugging this next.

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
 import argparse
 import json
+import sys
 from iterators.comment_iterator import CommentIterator
+
+def valid_arguments(argument_parser):
+    invalid_time_limit = (
+        ((argument_parser.hours != None) and (argument_parser.hours < 0)) or \
+        ((argument_parser.minutes != None) and (argument_parser.minutes < 0)) or \
+        ((argument_parser.seconds != None) and (argument_parser.seconds < 0))
+    )
+    invalid_comment_limit = (argument_parser.limit != None) and (argument_parser.limit <= 0)
+    if invalid_time_limit:
+        print(
+            'If time limits are specified, input for one of --hours, --minutes, and --seconds must be greater than 0. You entered a negative value for one of them. Exiting with an error code of 1.',
+            file=sys.stderr, flush=True
+        )
+        return False
+    elif invalid_comment_limit:
+        print(
+            'Input for the --limit parameter must be greater than 0. Exiting with an error code of 1.',
+            file=sys.stderr, flush=True
+        )
+        return False
+    return True
+
 
 def main():
     parser = argparse.ArgumentParser(
@@ -22,14 +45,25 @@ def main():
     parser.add_argument(
         '-o', '--output', type=str, default='comments.json', help='The output file you will store the JSON in. Defaults to "comments.json".'
     )
+    parser.add_argument(
+        '--hours', type=int, default=None, help='The maximum number of hours you want the program to run.'
+    )
+    parser.add_argument(
+        '--minutes', type=int, default=None, help='The maximum number of minutes you want the program to run.'
+    )
+    parser.add_argument(
+        '--seconds', type=int, default=None, help='The maximum number of seconds you want the program to run.'
+    )
     arguments = parser.parse_args()
+    if not valid_arguments(arguments):
+        exit(1)
     comments = {
         'comments': []
     }
-    with open(arguments.output, 'w') as output_file:
-        for item in CommentIterator(arguments.url, arguments.limit, arguments.pattern):
-            comments['comments'].append(item)
-        output_file.write(json.dumps(comments))
+    #with open(arguments.output, 'w') as output_file:
+    #    for item in CommentIterator(arguments.url, arguments.limit, arguments.pattern):
+    #        comments['comments'].append(item)
+    #    output_file.write(json.dumps(comments))
 
 if __name__ == '__main__':
     main()
